a8f5a35599a8cd90ae11e5096b87c4ef5465abe7
madhavihanchate <madhavihanchate5164@gmail.com>
2026-07-31 01:00:32 +0530

    Force committed solution - Time: O(1), Space: O(1)

--- 22727-write-a-function-Hackerrank/22727-write-a-function.py
+++ 22727-write-a-function-Hackerrank/22727-write-a-function.py
@@ -1,9 +1,12 @@
 def is_leap(year):
-    leap = False
-    
-    if year%4 == 0 and year%100 ==0 :
-        leap =True    
-    return leap
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
 
 
 
