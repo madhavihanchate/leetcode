65c7fbe9edec345ed7de74654399f9fb365f25e7
madhavihanchate <madhavihanchate5164@gmail.com>
2026-08-02 18:17:02 +0530

    Time: 0 ms (100%), Space: 12.3 MB (94.74%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 0877-stone-game/0877-stone-game.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def stoneGame(self, piles):
+        return True
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
